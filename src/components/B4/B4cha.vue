f7cdcb47e09b3b1d5427ee2625b7be3bd2bef916
lhn0911 <laihoangnam2005@gmail.com>
2024-10-03 07:48:32 +0700

    add

---
+++ src/components/B4/B4cha.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <p>age: {{ age }}</p>
+    <button @click="handleIncreaseAge">Increase age</button>
+    <B4Children :age="age"></B4Children>
+  </div>
+</template>
+
+<script setup>
+import B4Children from "./B4con.vue";
+import { ref } from "vue";
+
+const age = ref(1);
+const handleIncreaseAge = () => {
+  age.value++;
+};
+</script>
+
+<style></style>
